ff774f59f36fd8c8d511ed79e17da4bb9df9b677
Eric-szh <Eric.szh@hotmail.com>
2026-02-01 17:40:09 -0500

    Add CatBubble feature and update scenes
    Introduced the CatBubble script and associated sprite for a new cat bubble feature. Updated Main.unity to include the cat bubble GameObject with trigger logic, and made layout and text corrections in End.unity. Removed obsolete performance test resource files and incremented the project version to 1.2.0.

---
+++ Assets/Script/Animal/CatBubble.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CatBubble : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
